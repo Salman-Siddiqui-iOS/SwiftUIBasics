b6948544ee688b28e75356b0dbf0895a9c052d65
Salman Siddiqui <salman.siddiqui@healthify.co>
2021-06-19 16:25:25 +0530

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by salman siddiqui on 19/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
